e73517ba47f5420998ee32aff73ed63bc29fe760
kodam <kodam@YAMINI>
2024-01-25 16:54:19 +0530

    Add project files.

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherApp.Models;
+
+namespace WeatherApp.Controllers
+{
+    public class WeatherController : Controller
+    {
+
+        public List<CityWeather> weathers { get; set; }
+        public WeatherController()
+        {
+            weathers = WeatherData.GetData();
+        }
+        [Route("/")]
+        [Route("/weather")]
+        public IActionResult Index()
+        {
+            return View(weathers);
+        }
+        [Route("/weather/{cityCode}")]
+        public IActionResult Details([FromRoute]string cityCode)
+        {
+            CityWeather weather = weathers.FirstOrDefault(w => w.CityUniqueCode == cityCode);
+            if(weather == null)
+            {
+                return View("ErrorMessage");
+            }
+            return View(weather);
+        }
+    }
+    public static class WeatherData
+    {
+        public static List<CityWeather> GetData()
+        {
+            List<CityWeather> cityWeathers = new List<CityWeather>
+            {
+                new CityWeather() {
+                    CityUniqueCode = "LDN", CityName = "London", DateAndTime = new DateTime(2030, 01, 01, 8, 00, 00),  TemperatureFahrenheit = 33
+                },
+                new CityWeather() {
+                    CityUniqueCode = "NYC", CityName = "New York", DateAndTime = new DateTime(2030, 01, 01, 3, 00, 00),  TemperatureFahrenheit = 60
+                },
+                new CityWeather() {
+                    CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = new DateTime(2030, 01, 01, 9, 00, 00),  TemperatureFahrenheit = 82
+                }
+            };
+            return cityWeathers;
+        }
+    }
+}
